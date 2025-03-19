4af598fed106c822ac60db2184dcd3e7771220a2
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-03-18 21:25:34 -0300

    feat: Implementing the examples of the lessons: querySelector and differents approaches on different objects, and varible scopes.

---
+++ curso-javascript-projeto-usuarios/index.js
@@ -0,0 +1,19 @@
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked"); 
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry"); 
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector( "#exampleInputPassword"); 
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("exampleInputAdmin");
+
+console.table({
+    name,
+    gender,
+    birth,
+    country,
+    email,
+    password,
+    photo,
+    admin
+});
